14a3a2c1bc488b73e3ff5224f86a6d7954bba41f
Rahul Kumar Mahto <rahulkumarmahto@hotmail.com>
2024-05-24 00:12:58 +0530

    Initial checkin

---
+++ NewsPortal.WebAPI/NewsPortal.Models/NewsArticle.cs
@@ -0,0 +1,15 @@
+﻿namespace NewsPortal.Models
+{
+    public class NewsArticle
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public int CategoryId { get; set; }
+        public required string CreatedBy { get; set; }
+        public DateTime CreatedDatetime { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime ModifiedDatetime { get; set; }
+
+    }
+}
